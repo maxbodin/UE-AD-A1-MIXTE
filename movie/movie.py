9343fd2cbc5af772c2169cd5f6d606b4d8921a80
Maxime Bodin <maxime.bodin1@etudiant.univ-lr.fr>
2024-10-04 08:43:31 +0200

    movie.py : Refactoring constants in dedicated file + adding graphql props and endpoint.

--- movie/movie.py
+++ movie/movie.py
@@ -1,24 +1,44 @@
 from ariadne import graphql_sync, make_executable_schema, load_schema_from_path, ObjectType, QueryType, MutationType
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 
 import resolvers as r
+from constants import HOST, MOVIE_PORT
 
-PORT = 3001
-HOST = '0.0.0.0'
 app = Flask(__name__)
 
-# todo create elements for Ariadne
+# Intégration des types déclarés dans le schéma GraphQL.
+type_defs = load_schema_from_path('movie.graphql')
 
-# root message
+# Création des objets associés au schéma.
+query = QueryType()
+movie = ObjectType('Movie')
+
+# Association du resolver à la requête associée dans le schéma.
+query.set_field('movie_with_id', r.movie_with_id)
+
+# Création du schéma dit exécutable avec les éléments précédents.
+schema = make_executable_schema(type_defs, movie, query)
+
+
 @app.route("/", methods=['GET'])
 def home():
-    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>",200)
+    return make_response("<h1 style='color:blue'>Welcome to the Movie service!</h1>", 200)
 
-# graphql entry points
+
+# Graphql entry points
 @app.route('/graphql', methods=['POST'])
 def graphql_server():
-    # todo to complete
+    data = request.get_json()
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=None,
+        debug=app.debug
+    )
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
+
 
 if __name__ == "__main__":
-    print("Server running in port %s"%(PORT))
-    app.run(host=HOST, port=PORT)
+    print("Server running in port %s" % MOVIE_PORT)
+    app.run(host=HOST, port=MOVIE_PORT)
